591d558806dc67fd295a846bd41e1ab14c0accb2
isaqueDoSantos <139279858+isaqueDoSantos@users.noreply.github.com>
2023-10-26 15:39:38 -0300

    mais exercicios

---
+++ aulas-javascript/js/ex01/scripts.js
@@ -0,0 +1,15 @@
+window.alert('Minha Primeira Menssagem!!') /* esse comando emite uma mensagem*/
+window.confirm('Esta Gostando?')/*este comando faz uma pergunta de confirmaçao*/
+window.prompt('Qual sua Idade?')/*este comando faz uma pergunta de resposta*/
+
+
+// console 
+console.error("este é um erro!")
+console.warn('este é um aviso!')
+console.log('esta pe uma mensagem!')
+
+// variáveis
+var x=10
+// maneiras mais modernas
+let y=15
+const z =20
